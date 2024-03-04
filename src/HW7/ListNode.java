eafdd345e63562947994dd322ca9136cabffb8e1
Kirill Poliak <poliak.kirill@gmail.com>
2024-03-04 18:03:07 +0200

    Homework #7

---
+++ src/HW7/ListNode.java
@@ -0,0 +1,10 @@
+package HW7;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
---
+++ src/HW7/Solution.java
@@ -0,0 +1,26 @@
+package HW7;
+
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+
+        return prev;
+    }
+
+    public void printList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}

b8cede2cca93acf880ba04ce7c879cd9aecf9405
Kirill Poliak <poliak.kirill@gmail.com>
2024-03-04 17:12:02 +0200

    Homework #4

---
+++ src/HW4/AnagramCheck.java
@@ -0,0 +1,14 @@
+package HW4;
+import java.util.Arrays;
+public class AnagramCheck {
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+        return Arrays.equals(sChars, tChars);
+    }
+}
